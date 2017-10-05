edb525fb719b03d69752c0c32a8113fb698578cf
Susant Pant <susantpant@hotmail.com>
2017-10-05 04:11:09 -0600

    first commit, basic template

---
+++ Tester.java
@@ -0,0 +1,40 @@
+
+/**
+ * A simple test driver
+ * 
+ * @author 	Majid Ghaderi
+ * @version	3.2, Sep 22, 2017
+ *
+ */
+
+import java.io.IOException;
+
+public class Tester {
+	
+	public static void main(String[] args) {
+
+		// include whatever URL you like
+		// these are just some samples
+		String[] url = {"people.ucalgary.ca/~mghaderi/index.html",
+						"people.ucalgary.ca/~mghaderi/test/uc.gif",
+						"people.ucalgary.ca/~mghaderi/test/a.pdf",
+						"people.ucalgary.ca:80/~mghaderi/test/test.html"};
+		
+		// this is a very basic tester
+		// the TAs will use a more comprehensive set of tests
+		try {
+			UrlCache cache = new UrlCache();
+			
+			for (int i = 0; i < url.length; i++)
+				cache.getObject(url[i]);
+			
+			System.out.println("Last-Modified for " + url[0] + " is: " + cache.getLastModified(url[0]));
+			cache.getObject(url[0]);
+			System.out.println("Last-Modified for " + url[0] + " is: " + cache.getLastModified(url[0]));
+		}
+		catch (IOException e) {
+			System.out.println("There was a problem: " + e.getMessage());
+		}
+	}
+	
+}
---
+++ UrlCache.java
@@ -0,0 +1,44 @@
+
+/**
+ * UrlCache Class
+ * 
+ *
+ */
+
+import java.io.IOException;
+
+public class UrlCache {
+
+    /**
+     * Default constructor to initialize data structures used for caching/etc
+	 * If the cache already exists then load it. If any errors then throw runtime exception.
+	 *
+     * @throws IOException if encounters any errors/exceptions
+     */
+	public UrlCache() throws IOException {
+		
+	}
+	
+    /**
+     * Downloads the object specified by the parameter url if the local copy is out of date.
+	 *
+     * @param url	URL of the object to be downloaded. It is a fully qualified URL.
+     * @throws IOException if encounters any errors/exceptions
+     */
+	public void getObject(String url) throws IOException {
+		
+	}
+	
+    /**
+     * Returns the Last-Modified time associated with the object specified by the parameter url.
+	 *
+     * @param url 	URL of the object 
+	 * @return the Last-Modified time in millisecond as in Date.getTime()
+     */
+	public long getLastModified(String url) {
+		long millis = 0;
+		
+		return millis;
+	}
+
+}
